372e94b1e76e24a17639c47ae254ef7096d3809e
Arkaim <kaimbayev@gmail.com>
2018-09-02 15:42:50 +0600

    initial:

---
+++ index.js
@@ -0,0 +1,9 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var bot = new TelegramBot('640746920:AAGXCQjVubGbqsf-EHnTrwbVgQJ4ZDQ9DOk', {polling:true});
+
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+});
