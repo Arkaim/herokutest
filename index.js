75a00c0cd4ef809816bb91e2dccb793556a16642
Arkaim <kaimbayev@gmail.com>
2018-09-17 14:47:31 +0600

    top

--- index.js
+++ index.js
@@ -268,7 +268,6 @@ bot.onText(/\/top/, msg => {
 		var str = 'Топ 10 игроков: \n\n';
 		for (var i = 0; i < len; i++) {
 			str += (i + 1) + '. 😶 ' + players[i].fname + ' ' + players[i].lname + ', '
-							  + players[i].faculty + ', ' + players[i].year + ' курс, '
 							  + players[i].killcount + ' убийств' + '\n';
 		}
 		bot.sendMessage(msg.chat.id, str);
